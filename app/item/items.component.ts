57ad7f460927b3c3edda8e73672a27c9ff44ba50
quentinor <quentin.rondepierre@gmail.com>
2017-03-31 15:04:58 +0200

    dfgdfg

--- app/item/items.component.ts
+++ app/item/items.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 import { Item } from "./item";
 import { ItemService } from "./item.service";
+import {ObservableArray} from "data/observable-array";
+import {RadListViewComponent} from "nativescript-telerik-ui/listview/angular";
 
 @Component({
     selector: "ns-items",
@@ -10,10 +12,25 @@ import { ItemService } from "./item.service";
 })
 export class ItemsComponent implements OnInit {
     items: Item[];
+    private dataItems:ObservableArray<Item>;
 
     constructor(private itemService: ItemService) { }
 
-    ngOnInit(): void {
-        this.items = this.itemService.getItems();
+    ngOnInit() {
+
+
+
+
+        this.dataItems=new ObservableArray<Item>([
+            { id: 1, name: "Ter Stegen", role: "Goalkeeper" },
+            { id: 3, name: "Piqué", role: "Defender" },
+            { id: 4, name: "I. Rakitic", role: "Midfielder" },
+            { id: 5, name: "Sergio", role: "Midfielder" },
+            { id: 6, name: "Denis Suárez", role: "Midfielder" },
+            { id: 7, name: "Arda", role: "Midfielder" }
+        ]);
+        console.log(this.dataItems);
     }
+
+
 }
